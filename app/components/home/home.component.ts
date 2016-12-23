f032a948539da663bfd1fd3f06a1ecbcb3d79b04
Daniel Lim <hbt@markhamboards.com>
2016-12-22 19:16:25 -0500

    middle update
    middle update

--- app/components/home/home.component.ts
+++ app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Auth } from '../services/auth.service';
+import { Auth } from '../../services/auth.service';
 
 @Component({
   moduleId: module.id,
