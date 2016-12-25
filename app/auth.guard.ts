17d76dee9af874e2e68870f7e06232af1778c41a
Daniel Lim <hbt@markhamboards.com>
2016-12-25 00:00:09 -0500

    start Guard Auth
    start Guard Auth

---
+++ app/auth.guard.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate } from '@angular/router';
+import { Auth } from './services/auth.service';
+
+@Injectable() {
+
+    
+}
